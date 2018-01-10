b84814a9adddc33f433216b1fb47e2f1ae88c35c
kimburke55 <kimburke55@gmail.com>
2018-01-10 00:02:15 +0000

    Add files via upload

---
+++ src/main/java/ie/gmit/sw/Runner.java
@@ -0,0 +1,15 @@
+package ie.gmit.sw;
+
+/**
+ * Main class for running the Document Similarity 
+ * @author kimburke
+ *
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
